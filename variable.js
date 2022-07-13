a0ecff2391a07312183ed9b031900717b389b140
Rizal Maulana <rizalbeethoven@gmail.com>
2022-07-13 11:26:01 +0700

    jenis variable

---
+++ variable.js
@@ -0,0 +1,19 @@
+// Jenis Variable
+// String >> Char (a,1,$) '' "" 
+huruf = 'a,b,c';
+kata = 'satu, dua';
+angka = '1,2,3';
+// Number >> Integer, double, float
+bilangan = 1;
+desimal = 2.5;
+// Boolean >> True & False
+Boolean1 = true;
+Boolean2 = false;
+
+// Deklarasi variable
+// var, let, const
+// Var
+var angka = 1;
+var huruf = 'a';
+//let
+let angka = 1;
